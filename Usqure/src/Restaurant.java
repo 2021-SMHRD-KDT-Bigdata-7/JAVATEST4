c6d2ab94d52475292fcbbfdcb63a75859082bb6d
smhrd <smhrd@DESKTOP-S8D2MR1>
2021-10-21 12:21:05 +0900

    혜인커밋

--- Usqure/src/Restaurant.java
+++ Usqure/src/Restaurant.java
@@ -1,4 +1,23 @@
+public abstract class Restaurant extends Store{
+	
 
-public class Restaurant {
+	
+	double service;
+	double flavor;
+	double clean;
+	
+	public Restaurant(String name, String event, double price, double service, double flavor, double clean) {
+		super(name, event, price);
+		this.service = service;
+		this.flavor = flavor;
+		this.clean = clean;
+	}
 
+	@Override
+	public double grade() {
+		double result = 0;
+		result = Math.round((service+flavor+clean+ getPrice())*100/100.0)/4;
+		
+		return result;
+	}
 }
--- Usqure/src/Salon.java
+++ Usqure/src/Salon.java
@@ -1,4 +1,21 @@
 
-public class Salon {
+public class Salon extends Store{
 
+	double technology;
+	double kindness;
+	
+	public Salon(String name, String event, double price, double technology, double kindness) {
+		super(name, event, price);
+		this.technology = technology;
+		this.kindness = kindness;
+	}
+
+	@Override
+	public double grade() {
+		double result = ((technology + kindness + getPrice())*100/100.0)/3;
+		return result;
+	}
+	
+	
+	
 }
